1cb03d628c4dc5f902d12c3b169a0160e72fa24d
ugonna-onyejekwe <innocentugonna063@gmail.com>
2024-08-22 16:48:29 +0100

    Edited hero section  (Added swiper for the images)

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,9 +1,20 @@
 import "./hero.scss";
-// import home2 from "../../assets/hero2.webp";
-import home2 from "../../assets/retail.png";
+import hero1 from "../../assets/hero1.png";
+import hero2 from "../../assets/hero2.png";
+import hero3 from "../../assets/hero3.png";
+import hero4 from "../../assets/hero4.png";
 import getAppG from "../../assets/download-g.png";
 import getAppA from "../../assets/download-a.png";
 import { Link } from "react-router-dom";
+
+// swiper
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectCoverflow } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/autoplay";
 
 export const Hero = () => {
   return (
@@ -37,9 +48,34 @@ export const Hero = () => {
         </div>
 
         <div className="img_con" data-aos="zoom-in">
-          <div className="img">
-            <img src={home2} alt="user image" />
-          </div>
+          <Swiper
+            modules={[EffectCoverflow, Autoplay]}
+            effect="coverflow"
+            grabCursor={true}
+            autoplay={{
+              delay: 2000,
+            }}
+            speed={700}
+            centeredSlides={true}
+            loop={true}
+            slidesPerView={"auto"}
+            coverflowEffect={{
+              rotate: 0,
+              stretch: 0,
+              depth: 200,
+              modifier: 5,
+            }}
+          >
+            <SwiperSlide className="img">
+              <img src={hero1} alt="user image" />
+            </SwiperSlide>
+            <SwiperSlide className="img">
+              <img src={hero2} alt="user image" />
+            </SwiperSlide>
+            <SwiperSlide className="img">
+              <img src={hero3} alt="user image" />
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
     </div>
