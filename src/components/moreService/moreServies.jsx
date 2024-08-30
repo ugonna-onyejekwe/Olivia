c73867183482b40ecd1f0edf67408dff76e73e2e
ugonna-onyejekwe <innocentugonna063@gmail.com>
2024-08-30 04:20:44 +0100

    Edited more services section

--- src/components/moreService/moreServies.jsx
+++ src/components/moreService/moreServies.jsx
@@ -7,8 +7,10 @@ import { Autoplay } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/autoplay";
+import { useState } from "react";
 
 export const MoreServices = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
   return (
     <div className="moreServices_section">
       <div className="container">
@@ -17,12 +19,15 @@ export const MoreServices = () => {
             modules={[Autoplay]}
             grabCursor={true}
             autoplay={{
-              delay: 2000,
+              delay: 3000,
             }}
-            speed={700}
+            speed={1000}
             centeredSlides={true}
             slidesPerView={"auto"}
             spaceBetween={30}
+            onSlideChange={(swiper) => {
+              setCurrentSlide(swiper.activeIndex);
+            }}
           >
             <SwiperSlide className="img">
               <img src={image1} alt="user image" />
@@ -37,21 +42,21 @@ export const MoreServices = () => {
         </div>
 
         <div className="txt_con">
-          <div className="box">
+          <div className={currentSlide === 0 ? "box active" : "box"}>
             <h3> Booking Services</h3>
             <p>
               Add a booking service to your custom Olivia Stores website. Easily
               manage appointments and reservations.
             </p>
           </div>
-          <div className="box">
+          <div className={currentSlide === 1 ? "box active" : "box"}>
             <h3> Digital Products</h3>
             <p>
               Sell digital products effortlessly with Olivia Stores. Our
               platform simplifies online sales.
             </p>
           </div>
-          <div className="box">
+          <div className={currentSlide === 2 ? "box active" : "box"}>
             <h3> Events </h3>
             <p>
               Easily add upcoming events to your Olivia Stores website. Share
